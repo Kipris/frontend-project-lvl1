1190171e44d715e579a33225bfddc430700d22c7
Kipris <tnzaria@gmail.com>
2020-07-11 16:54:16 +0300

    add .editorconfig

--- src/games/gcdGame.js
+++ src/games/gcdGame.js
@@ -3,26 +3,26 @@ import Game from './game.js';
 import { getRandomNumber } from '../utils.js';
 
 const getGcd = (firstNumber, secondNumber) => {
-  if (!secondNumber) {
-    return firstNumber;
-  }
-  return getGcd(secondNumber, firstNumber % secondNumber);
+	if (!secondNumber) {
+		return firstNumber;
+	}
+	return getGcd(secondNumber, firstNumber % secondNumber);
 };
 
 const gcdGame = new Game();
 
 gcdGame.logConditions = function logConditions() {
-  console.log('Find the greatest common divisor of given numbers.');
+	console.log('Find the greatest common divisor of given numbers.');
 };
 
 gcdGame.executeGame = function executeGame() {
-  const firstNumber = getRandomNumber();
-  const secondNumber = getRandomNumber();
-  console.log(`Question: ${firstNumber} ${secondNumber}`);
-  const answer = readlineSync.question('Your answer: ');
-  const correctAnswer = getGcd(firstNumber, secondNumber);
-  this.setAnswer(Number(answer));
-  this.setCorrectAnswer(Number(correctAnswer));
+	const firstNumber = getRandomNumber();
+	const secondNumber = getRandomNumber();
+	console.log(`Question: ${firstNumber} ${secondNumber}`);
+	const answer = readlineSync.question('Your answer: ');
+	const correctAnswer = getGcd(firstNumber, secondNumber);
+	this.setAnswer(Number(answer));
+	this.setCorrectAnswer(Number(correctAnswer));
 };
 
 export default gcdGame;
